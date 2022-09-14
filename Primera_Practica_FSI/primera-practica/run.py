33a6976c03240bec3016b852885e52ae34a1585c
Joi Berberi <joi9719@hotmail.it>
2022-09-14 18:46:05 +0200

    Add files via upload

---
+++ Primera_Practica_FSI/primera-practica/run.py
@@ -0,0 +1,14 @@
+# Search methods
+
+import search
+
+ab = search.GPSProblem('A', 'Z', search.romania)
+
+print(search.breadth_first_graph_search(ab).path())
+print(search.depth_first_graph_search(ab).path())
+
+print(search.ramificación_y_acotación(ab).path())
+print(search.ramificación_y_acotación_con_subestimación(ab).path())
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
